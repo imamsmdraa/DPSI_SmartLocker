22a12d3a7069c382ab97e2ca93705a5781b1744b
NaufalFadhli <2300016018@webmail.uad.ac.id>
2025-06-13 20:45:11 +0700

    naufal dpsi

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+// main.dart
+import 'package:flutter/material.dart';
+import 'screens/register/sign_in.dart';
+import 'screens/register/create_account.dart';
+import 'screens/register/email_verification.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+ 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Smart Locker',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Inter',
+        useMaterial3: true,
+      ),
+      initialRoute: '/sign-in',
+      routes: {
+        '/sign-in': (context) => const SignInScreen(),
+        '/create-account': (context) => const CreateAccountScreen(),
+        '/email-verification': (context) => const EmailVerificationScreen(),
+      },
+    );
+  }
+}
