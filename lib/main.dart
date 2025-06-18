1f1db379e696c85f1c303d2c1116478750fb7b35
RHDistroyal <rohidadepratama@gmail.com>
2025-06-18 22:06:24 +0700

    Simpan perubahan lokal

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,6 @@ import 'screens/register/history.dart';
 import 'screens/register/help_and_support.dart';
 import 'screens/register/profile.dart';
 import 'screens/register/openning.dart';
-import 'screens/register/home_screen.dart';
-import 'screens/register/payment_screen.dart';
-import 'screens/register/success_screen.dart';
-import 'screens/register/language.dart';
-
 
 
 void main() {
@@ -39,10 +34,6 @@ class MyApp extends StatelessWidget {
         '/help_and_support': (context) => const HelpAndSupportScreen(),
         '/profile': (context) => const ProfileScreen(),
         '/openning': (context) => const OpenningScreen(),
-        '/home_screen': (context) => const HomeScreen(),
-        '/payment_screen': (context) => const PaymentScreen(),
-        '/success_screen': (context) => const SuccessScreen(),
-        '/language': (context) => const FigmaToCodeApp(),
 
       },
     );
