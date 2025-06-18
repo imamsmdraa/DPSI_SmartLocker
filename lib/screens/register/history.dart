72d51094bda22290cb938566b5d599306023aa9a
falishaalf <2300016128@webmail.uad.ac.id>
2025-06-19 00:21:50 +0700

    nanti benerin lagi

--- lib/screens/register/history.dart
+++ lib/screens/register/history.dart
@@ -12,7 +12,7 @@ class HistoryScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('History'),
-        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        backgroundColor: Colors.white,
       ),
       body: Column(
         children: [
@@ -35,180 +35,269 @@ class ProfileUser extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        width: double.infinity,
-        padding: const EdgeInsets.all(20),
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color(0xFF50BF95),
-              Color(0xFF39705B),
-            ],
+      child: Center(
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(20),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFF50BF95),
+                Color(0xFF39705B),
+              ],
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(30)),
           ),
-          borderRadius: BorderRadius.vertical(bottom: Radius.circular(30)),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Container Putih Besar (Gradient + History Title + Active Booking)
-            Container(
-              width: 343,
-              height: 558,
-              child: Stack(
-                children: [
-                  // Background Gradient Putih
-                  Positioned(
-                    left: 0,
-                    top: 0,
-                    child: Container(
-                      width: 343,
-                      height: 558,
-                      decoration: ShapeDecoration(
-                        gradient: const LinearGradient(
-                          begin: Alignment(0.50, -0.00),
-                          end: Alignment(0.50, 1.00),
-                          colors: [Colors.white, Color(0xFF50BD93)],
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        shadows: const [
-                          BoxShadow(
-                            color: Color(0x0F000000),
-                            blurRadius: 44,
-                            offset: Offset(0, 4),
-                            spreadRadius: 0,
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  // Title dan Elemen
-                  Positioned(
-                    left: 17,
-                    top: 31.55,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Transform.rotate(
-                          angle: 1.57,
-                          child: const SizedBox(
-                            width: 15,
-                            height: 25,
-                            child: Stack(),
-                          ),
-                        ),
-                        const Text(
-                          'History',
-                          style: TextStyle(
-                            color: Color(0xFF181D27),
-                            fontSize: 30,
-                            fontFamily: 'DM Sans',
-                            fontWeight: FontWeight.w700,
-                            height: 0.65,
-                          ),
-                        ),
-                        const SizedBox(height: 25),
-                        Container(
-                          width: 40,
-                          height: 40,
-                          decoration: const ShapeDecoration(
-                            color: Color(0xFF39705B),
-                            shape: OvalBorder(),
-                          ),
-                        ),
-                        const SizedBox(height: 25),
-                        const SizedBox(
-                          width: 20,
-                          height: 20,
-                          child: Stack(),
-                        ),
-                      ],
-                    ),
-                  ),
-                  // Active Booking di dalam container putih
-                  Positioned(
-                    left: 10,
-                    top: 150,
-                    child: Container(
-                      width: 320,
-                      height: 350,
-                      child: Stack(
-                        children: [
-                          Container(
-                            width: double.infinity,
-                            height: double.infinity,
-                            decoration: const BoxDecoration(
-                              image: DecorationImage(
-                                image: AssetImage('assets/images/history.png'),
-                                fit: BoxFit.cover,
-                              ),
-                              borderRadius: BorderRadius.all(Radius.circular(10)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Container(
+                  width: 343,
+                  height: 558,
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.center,
+                        child: Container(
+                          width: 343,
+                          height: 558,
+                          decoration: ShapeDecoration(
+                            gradient: const LinearGradient(
+                              begin: Alignment(0.50, -0.00),
+                              end: Alignment(0.50, 1.00),
+                              colors: [Colors.white, Color(0xFF50BD93)],
                             ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            shadows: const [
+                              BoxShadow(
+                                color: Color(0x0F000000),
+                                blurRadius: 44,
+                                offset: Offset(0, 4),
+                                spreadRadius: 0,
+                              )
+                            ],
                           ),
-                          const Positioned(
-                            top: 20,
-                            left: 20,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                        ),
+                      ),
+                      Positioned(
+                        left: 17,
+                        top: 31.55,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
-                                Text(
-                                  'Active Booking',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
+                                Container(
+                                  width: 40,
+                                  height: 40,
+                                  decoration: const ShapeDecoration(
+                                    color: Color(0xFF39705B),
+                                    shape: OvalBorder(),
                                   ),
                                 ),
-                                SizedBox(height: 10),
-                                Text(
-                                  'Kampus UAD 4',
-                                  style: TextStyle(fontSize: 16, color: Colors.white),
-                                ),
-                                SizedBox(height: 10),
-                                Text(
-                                  'A1',
+                                const SizedBox(width: 12),
+                                const Text(
+                                  'History',
                                   style: TextStyle(
-                                    fontSize: 96,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
+                                    color: Color(0xFF181D27),
+                                    fontSize: 30,
+                                    fontFamily: 'DM Sans',
+                                    fontWeight: FontWeight.w700,
+                                    height: 0.65,
                                   ),
                                 ),
-                                SizedBox(height: 20),
-                                Text(
-                                  'Expired in',
-                                  style: TextStyle(fontSize: 16, color: Colors.white),
-                                ),
-                                Text(
-                                  '3 : 59 : 10',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                SizedBox(height: 20),
-                                Text(
-                                  'Rp. 15.000',
-                                  style: TextStyle(
-                                    fontSize: 25,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                  ),
+                                const SizedBox(width: 120),
+                                Image.asset(
+                                  'assets/images/panah.png',
+                                  width: 20,
+                                  height: 20,
                                 ),
                               ],
                             ),
+                            const SizedBox(height: 25),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        left: 10,
+                        top: 100,
+                        child: Container(
+                          width: 320,
+                          height: 250,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: const DecorationImage(
+                              image: AssetImage('assets/images/history.png'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          padding: const EdgeInsets.all(20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: const [
+                                      Text(
+                                        'Active Booking',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      SizedBox(height: 10),
+                                      Text(
+                                        'Kampus UAD 4',
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(
+                                    width: 108,
+                                    height: 111,
+                                    child: Text(
+                                      'A1',
+                                      textAlign: TextAlign.right,
+                                      style: TextStyle(
+                                        fontSize: 80,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                        fontFamily: 'DM Sans',
+                                        shadows: [
+                                          Shadow(
+                                            offset: Offset(0, 4),
+                                            blurRadius: 4,
+                                            color: Color(0xFF000000),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 20),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: const [
+                                      Text(
+                                        'Expired in',
+                                        style: TextStyle(fontSize: 16, color: Colors.white),
+                                      ),
+                                      SizedBox(height: 5),
+                                      Text(
+                                        '3 : 59 : 10',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const Text(
+                                    'Rp. 15.000',
+                                    style: TextStyle(
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              // Tambahan Logo, Teks Smart Locker & Slogan
+              Container(
+                width: 72,
+                height: 55,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 26,
+                      top: 0,
+                      child: Container(
+                        width: 22.31,
+                        height: 23,
+                        decoration: BoxDecoration(
+                          image: const DecorationImage(
+                            image: AssetImage('assets/images/logo1.png'),
+                            fit: BoxFit.fill,
+                          ),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Color(0x19000000),
+                              blurRadius: 2.10,
+                              offset: Offset(2, 3),
+                              spreadRadius: 0,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    const Positioned(
+                      left: 0,
+                      top: 25,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Smart Locker',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 11,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w800,
+                              height: 1.40,
+                              letterSpacing: -0.11,
+                            ),
+                          ),
+                          SizedBox(height: 2),
+                          Text(
+                            'No Keys, No Hassle, Just Click !',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 8,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w300,
+                              height: 1.40,
+                              letterSpacing: 0.56,
+                            ),
                           ),
                         ],
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
 
-            const SizedBox(height: 30), // Jarak dengan bawah
-          ],
+              const SizedBox(height: 30),
+            ],
+          ),
         ),
       ),
     );
